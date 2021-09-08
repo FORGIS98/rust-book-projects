af477896316c8c1d9a40916dd60e4523e58b1cb5
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-09-08 11:24:42 +0200

    accepting command line args

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,11 @@
+use std::env; // To read command-line args
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // &args[0] = program name
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Search for `{}` in `{}`", query, filename);
 }
