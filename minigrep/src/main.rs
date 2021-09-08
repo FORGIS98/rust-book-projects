6bebbce0c7219297e254bb4c66c1c48fae8598fc
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-09-08 12:47:19 +0200

    args error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     let file_content = fs::read_to_string(config.filename).expect("Error reading the file.");
 
@@ -16,10 +19,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("expecting 2 arguments: `String` and `File`");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
