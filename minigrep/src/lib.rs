6f96bc1e5e75df9054789300e8306f8b075de669
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-09-09 20:54:12 +0200

    case insensitive option using env var

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,17 +1,25 @@
-use std::{error::Error, fs};
+use std::{error::Error, fs, env};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let file_content = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &file_content) {
+    let result = if config.case_sensitive {
+        search(&config.query, &file_content)
+    } else {
+        search_case_insensitive(&config.query, &file_content)
+    };
+
+    for line in result {
         println!("{}", line);
     }
+
     Ok(())
 }
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -22,8 +30,14 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        // is_err() returns false if the env var exists, no matter the value
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -38,18 +52,42 @@ pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
     all_matches
 }
 
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut all_matches = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            all_matches.push(line);
+        }
+    }
+
+    all_matches
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
-        let contents = "Rust is a programming language\nit's very safe, fast and productive.\nYou should try it!";
+        let contents = "Rust is\na programming language\nit's very safe, fast and productive.\nYou should try it!\nDuct tape.";
 
         assert_eq!(
             vec!["it's very safe, fast and productive."],
             search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "ruST";
+        let contents = "Rust is\na programming language\nit's very safe, fast and productive.\nYou should try it!\nTrust me.";
+
+        assert_eq!(
+            vec!["Rust is", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
